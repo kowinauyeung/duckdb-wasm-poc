b9a1f58ff9abaf1e5ac160d22a71065be9a479ad
kowinauyeung <kowinauyeung@gmail.com>
2024-10-18 18:58:16 +0900

    create, insert and select from table

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,84 @@
-import { useState } from "react";
+import { useState, useEffect, useRef, useCallback } from "react";
+import { AsyncDuckDBConnection } from "@duckdb/duckdb-wasm";
+import { getDB } from "./duckdbEntry";
 import "./App.css";
 
+type Row = {
+  id: string;
+  name: string;
+  email: string;
+  age: number;
+};
+
 function App() {
-  const [count, setCount] = useState(0);
+  const connRef = useRef<AsyncDuckDBConnection | null>(null);
+  const [ready, setReady] = useState(false);
+  const [rows, setRows] = useState<Row[]>([]);
+
+  const addRow = useCallback(async () => {
+    if (!ready || !connRef.current) return;
+    await connRef.current.query(`
+      INSERT INTO test
+      VALUES ('${new Date().getUTCMilliseconds()}', 'kowin', 'kowin@gmail.com', 18);
+    `);
+    const result = await connRef.current.query(`
+      SELECT *
+      FROM test
+    `);
+    const dbRows = result.toArray().map((row) => {
+      const parsedRow = JSON.parse(row);
+      return parsedRow;
+    });
+    setRows(dbRows);
+  }, [ready]);
+
+  useEffect(() => {
+    getDB().then(async (db) => {
+      const conn = await db.connect();
+      connRef.current = conn;
+      await conn.query(`
+        CREATE TABLE test (
+          id VARCHAR,
+          name VARCHAR,
+          email VARCHAR,
+          age INTEGER
+        );
+      `);
+      setReady(true);
+    });
+    return () => {
+      if (connRef.current) {
+        connRef.current.close();
+      }
+    };
+  }, []);
 
   return (
     <div>
       <div> </div>
       <h1>DuckDB WASM POC</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        {ready && <button onClick={addRow}>Add row</button>}
+      </div>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>name</th>
+              <th>email</th>
+              <th>age</th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map((row) => (
+              <tr key={row.id}>
+                <td>{row.name}</td>
+                <td>{row.email}</td>
+                <td>{row.age}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
