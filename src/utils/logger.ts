19e6e18ea9421472c777447229cc164969b2eb6e
kowinauyeung <kowinauyeung@gmail.com>
2024-10-23 18:14:24 +0900

    split file if reach max num of record

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -2,11 +2,11 @@ import { getDirectoryFromPath, getFileFromPath } from './opfsHelper'
 import { format, parseISO, isBefore, subDays } from 'date-fns'
 
 const LOG_DIR = 'logs'
-const FILE_NAME = 'log'
 const LOG_EXPIRES_IN_DAY = 30
+const MAX_RECORD_IN_FILE = 50000
 
-export const getLogFilePath = () =>
-  `${LOG_DIR}/${FILE_NAME}_${format(new Date(), 'yyyyMMdd')}.jsonl`
+export const getLogFilePath = (part: number = 1) =>
+  `${LOG_DIR}/${format(new Date(), 'yyyyMMdd')}_${part}.jsonl`
 
 export enum LOG_TYPE {
   RTC_STATS = 'rtc_stats',
@@ -38,8 +38,21 @@ export const removeExpiredLogFile = async () => {
   }
 }
 
+// this func limit each log file contain 50k line of record
+const getLogFile = async (i = 1) => {
+  const handle = await getFileFromPath(getLogFilePath(i))
+  const currContent = await handle.getContent()
+  const numOfLine = currContent.split('\n').length
+  console.log(numOfLine)
+  if (numOfLine < MAX_RECORD_IN_FILE) {
+    return handle
+  } else {
+    return await getLogFile(i + 1)
+  }
+}
+
 export const writeLog = async (logs: Log[]) => {
-  const handle = await getFileFromPath(getLogFilePath())
+  const handle = await getLogFile()
   const currContent = await handle.getContent()
   const currLogs = currContent.length > 0 ? `${currContent}\n` : ''
   const newLogs = logs.map((log) => JSON.stringify(log)).join('\n')
